db7fe99c182261fdd3a3081123f613d0f58a1b79
Tim Ansell <tim@thousandparsec.net>
2005-12-30 22:57:26 +0000

    Setup file.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+version = "0.0.1"
+
+import os.path
+import os
+
+setup(name="libtpclient-py",
+	version=version,
+	license="GPL",
+	description="Client support library for Thousand Parsec",
+	author="Tim Ansell",
+	author_email="tim@thousandparsec.net",
+	url="http://www.thousandparsec.net",
+	packages=[ \
+		'tp',
+		'tp.client',
+		],
+	package_dir = {'tp': ''}
+)
+
+

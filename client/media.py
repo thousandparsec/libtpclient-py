c54c73ca40e3e5632e863c8003f433cc7f73ff5c
Tim Ansell <tim@thousandparsec.net>
2006-07-07 19:52:49 +0000

    Added support for media downloading.

---
+++ client/media.py
@@ -0,0 +1,177 @@
+
+import os, os.path
+import shutil
+import stat
+import string
+
+import httplib
+import urllib, urlparse
+import time
+
+import gzip
+
+from cache import Cache
+
+class Media:
+	def __init__(self, key, serverurl, configdir=None, mediatypes=[], new=False, noconnect=False):
+		"""\
+		It is important that key constructed the following way,
+			protocol://username@server:port/
+		Everything must be there, even if the port is the default.
+
+		serverurl is the URL of the server to download the media from
+
+		configdir is where the cache will be stored.
+		mediatypes is the type of media which the client needs.
+		"""
+		if configdir == None:
+			configdir = Cache.configdir()
+
+		if not os.path.exists(configdir):
+			os.mkdir(configdir)
+
+		key = Cache.configkey(key)
+
+		self.dir = os.path.join(configdir, "media.%s" % (key,))
+
+		if os.path.exists(self.dir) and new:
+			shutil.rmtree(self.dir)
+		if not os.path.exists(self.dir):
+			os.mkdir(self.dir)
+
+		self.noconnect = noconnect
+
+		self.serverurl = serverurl
+		self.connection
+
+		self.gotten = []
+
+	def connection(self):
+		if not hasattr(self, '_connection'):
+			type, host, self.basepath, t, t, t = urlparse.urlparse(self.serverurl)
+			print type, host, self.basepath
+			self._connection = getattr(httplib, "%sConnection" % type.upper())(host)
+		return self._connection
+	connection = property(connection)
+
+	def getter(self):
+		if not hasattr(self, '_getter'):
+			self._getter = urllib.FancyURLopener()
+		return self._getter
+	getter = property(getter)
+
+	def updated(self, media_remote, media_local):
+		"""\
+		Checks the file's timestamp against the remote's version.
+		"""
+		self.connection.request("HEAD", media_remote)
+			
+		headers = {}
+		for key, value in self.connection.getresponse().getheaders():
+			headers[key] = value
+
+		remotedate = time.strptime(headers['last-modified'], "%a, %d %b %Y %H:%M:%S %Z")[0:5]
+		localdate = eval(open(media_local + ".timestamp").read())
+
+		print "Remote Date", remotedate
+		print "Local Date ", localdate
+
+		if remotedate <= localdate:
+			return False
+		return True
+
+	def ready(self, file, timestamp):
+		"""\
+		Is a file ready to be used?
+		"""
+		media_local = os.path.join(self.dir, file)
+		if os.path.exists(media_local):
+			if timestamp <= eval(open(media_local + ".timestamp").read()):
+				return media_local
+
+	def getfile(self, file, timestamp=None, callback=None):
+		"""\
+		Get a file, if it exists in the cache it is checked for freshness.
+		"""
+		if callback is None:
+			def callback(*args):
+				print args
+
+		media_local = os.path.join(self.dir, file)
+		media_remote = urlparse.urljoin(self.basepath, file)
+		media_url = urlparse.urljoin(self.serverurl, file)
+
+		print "Local  file", media_local, os.path.exists(media_local)
+		print "Remote file", media_remote
+		print "URL of file", media_url
+		if os.path.exists(media_local):
+			if timestamp is None:
+				if not self.updated(media_remote, media_local):
+					return media_local
+			else:
+				if not self.ready(file, timestamp) is None:
+					return media_local
+
+		if self.noconnect:
+			raise IOError("Could not get the file as in no connection mode!")	
+	
+		ldir = os.path.dirname(media_local)
+		if not os.path.exists(ldir):
+			os.makedirs(ldir)
+
+		(trash, message) = self.getter.retrieve(media_url, media_local, callback)
+		remotedate = time.strptime(message.getheader('last-modified'), "%a, %d %b %Y %H:%M:%S %Z")[0:5]
+
+		open(media_local + ".timestamp", 'w').write(repr(remotedate))
+		return media_local
+
+	files = "media.gz"
+	def getpossible(self, valid_types, callback=None):
+		"""
+		Gets the Media description file from the http server.
+		"""
+		file = self.getfile(self.files, callback)
+
+		for line in gzip.GzipFile(file, 'r').readlines():
+			line, timestamp = line.strip().split(' ')
+			timestamp = time.strptime(timestamp, "%Y%m%dT%H%M")[0:5]
+			for type in valid_types:
+				if line.endswith(type):
+					yield line, timestamp
+
+if __name__ == "__main__":
+	import sys
+	media_cache = Media(sys.argv[1], sys.argv[2])
+
+	files = {}
+	for file, timestamp in media_cache.getpossible(['png', 'gif', 'jpg']):
+		def splitall(start):
+			bits = []
+
+			while True:
+				start, end = os.path.split(start)
+				if end is '':
+					break
+				bits.append(end)
+			bits.reverse()
+			return bits
+
+		bits = splitall(file)
+		if bits[-2] in ['animation', 'still']:
+			type = bits[-2]
+			del bits[-2]
+		else:
+			type = 'still'
+
+		if not bits[-2].endswith("-small"):
+			continue
+		else:
+			key = bits[-2][:-6]
+			if not files.has_key(key):
+				files[key] = {}
+			if not files[key].has_key(type):
+				files[key][type] = []
+			files[key][type].append((file, timestamp))
+
+	import pprint
+	pprint.pprint(files)
--- client/threads.py
+++ client/threads.py
@@ -9,6 +9,8 @@ import traceback
 from tp.netlib import Connection, failed
 
 from cache import Cache
+from media import Media
+
 from config import load_data, save_data
 from version import version
 
@@ -21,8 +23,15 @@ class Application(object):
 	"""
 	
 	def __init__(self):
+		print self.GUIClass, self.NetworkClass, self.MediaClass
 		self.gui = self.GUIClass(self)
 		self.network = self.NetworkClass(self)
+		if not self.MediaClass is None:
+			self.media = self.MediaClass(self)
+		else:
+			self.media = None
+
+		print self.gui, self.network, self.media
 
 		self.cache = None
 		
@@ -33,7 +42,15 @@ class Application(object):
 		"""\
 		Set the application running.
 		"""
+		print "Application.Run"
+		print self.gui, self.network, self.media
 		self.network.start()
+		if not self.media is None:
+			print "Media Start..."
+			self.media.start()
+		else:
+			print "Not Media Start..."
+
 		self.gui.start()
 
 	def ConfigSave(self):
@@ -57,7 +74,9 @@ class Application(object):
 		"""\
 		Post an application wide event to every thread.
 		"""
+		print "Application.Post", event
 		self.network.Call(self.network.Post, event)
+		self.media.Call(self.media.Post, event)
 		self.gui.Call(self.gui.Post, event)
 
 	def Exit(self):
@@ -65,6 +84,7 @@ class Application(object):
 		Exit the program.
 		"""
 		self.network.Call(self.network.Cleanup)
+		self.media.Call(self.media.Cleanup)
 		self.gui.Call(self.gui.Cleanup)
 
 class NetworkThread(threading.Thread):
@@ -86,6 +106,7 @@ class NetworkThread(threading.Thread):
 		self.connection = Connection()
 	
 	def run(self):
+		print "Network.Run"
 		while not self.exit:
 			if len(self.tocall) <= 0:
 				if hasattr(time, 'sleep'):
@@ -93,6 +114,7 @@ class NetworkThread(threading.Thread):
 				continue
 			
 			method, args, kw = self.tocall.pop(0)
+			print "Network", method
 			try:
 				method(*args, **kw)
 			except (IOError, socket.error), e:
@@ -137,7 +159,7 @@ class NetworkThread(threading.Thread):
 			return False
 			
 		callback("Looking for Thousand Parsec Server...")
-		if failed(self.connection.connect(("libtpclient-py/%i.%i.%i " % version)+cs)):
+		if failed(self.connection.connect(("libtpclient-py/%s.%s.%s " % version)+cs)):
 			s  = _("The client connected to the host but it did not appear to be a Thousand Parsec server.\n")
 			s += _("This could be because the server is down or the connection details are incorrect.\n")
 			self.application.Post(self.NetworkFailureEvent(s))
@@ -226,3 +248,119 @@ class NetworkThread(threading.Thread):
 			self.application.Post(self.NetworkFailureEvent(e))
 			"There where the following errors when trying to send changes to the server:"
 			"The following updates could not be made:"
+
+
+class MediaThread(threading.Thread):
+	## These are network events
+	class MediaFailureEvent(Exception):
+		"""\
+		Raised when the media connection fails for what ever reason.
+		"""
+		pass
+
+	class MediaDownload:
+		def __init__(self, file, progress=0, size=0, localfile=None):
+			self.file = file
+			self.progress = progress
+			self.size = size
+			self.localfile = localfile
+
+	class MediaDownloadStartEvent(MediaDownload):
+		"""\
+		Posted when a piece of media is started being downloaded.
+		"""
+		pass
+
+	class MediaDownloadProgressEvent(MediaDownload):
+		"""\
+		Posted when a piece of media is being downloaded.
+		"""
+		pass
+
+	class MediaDownloadDoneEvent(MediaDownload):
+		"""\
+		Posted when a piece of media has been downloaded.
+		"""
+		pass
+
+	class MediaUpdateEvent:
+		"""\
+		Posted when the media was download.
+		"""
+		def __init__(self, files):
+			self.files = files
+
+	######################################
+	def __init__(self, application):
+		print "Media.__init__"
+		threading.Thread.__init__(self)
+		self.application = application
+		self.exit = False
+
+		self.tocall = []
+	
+	def run(self):
+		while not self.exit:
+			if len(self.tocall) <= 0:
+				if hasattr(time, 'sleep'):
+					time.sleep(0.1)
+				continue
+			
+			method, args, kw = self.tocall.pop(0)
+			print "Media.Run", method
+			try:
+				method(*args, **kw)
+			except IOError, e:
+				s  = _("There was an unknown network error.\n")
+				s += _("Any changes since last save have been lost.\n")
+				self.application.Post(self.MediaFailureEvent(s))
+
+	def Cleanup(self):
+		self.exit = True
+
+	def Call(self, method, *args, **kw):
+		"""\
+		Call a method in this thread.
+		"""
+		self.tocall.append((method, args, kw))
+
+	def Post(self, event):
+		"""
+		Post an Event the current window.
+		"""
+		func = 'On' + event.__class__.__name__[:-5]
+		print "Posting %s to %s" % (event, func)
+		if hasattr(self, func):
+			getattr(self, func)(event)
+
+	def GetFileGoing(self, file, timestamp):
+		"""\
+		"""
+		def callback(blocknum, blocksize, size, self=self, file=file):
+			progress = min(blocknum*blocksize, size)
+			if blocknum == 0:
+				self.application.Post(self.MediaDownloadStartEvent(file, progress, size))
+
+			self.application.Post(self.MediaDownloadProgressEvent(file, progress, size))
+
+		localfile = self.cache.getfile(file, timestamp, callback=callback)
+		self.application.Post(self.MediaDownloadDoneEvent(file, localfile=localfile))
+
+	def GetFile(self, file, timestamp):
+		"""\
+		Get a File, return directly or start a download.
+		"""
+		if self.cache.ready(file, timestamp):
+			return self.cache.getfile(file, timestamp)
+		self.Call(self.GetFileGoing, file, timestamp)
+
+	def ConnectTo(self, host, username, debug=False):
+		"""\
+		ConnectTo 
+		"""
+		self.cache = Media(Cache.key(host, username), "http://darcs.thousandparsec.net/repos/media/client/")
+
+		files = self.cache.getpossible(['png', 'gif'])
+		self.application.Post(self.MediaUpdateEvent(files))
+
+

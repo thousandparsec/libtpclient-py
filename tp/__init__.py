01322a31ffd5b011f03a1db35b83944e50687546
Tim Ansell <tim@thousandparsec.net>
2006-12-16 13:36:44 +0000

    Fixed problem for people without setuptools.

--- tp/__init__.py
+++ tp/__init__.py
@@ -1,5 +1,5 @@
-__import__('pkg_resources').declare_namespace(__name__)
 try:
+	__import__('pkg_resources').declare_namespace(__name__)
 	import modulefinder
 	for p in __path__:
 		modulefinder.AddPackagePath(__name__, p)

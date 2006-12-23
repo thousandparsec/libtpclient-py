2959c2bc2f295aaef1fc22231af7cab10234a908
Tim Ansell <tim@thousandparsec.net>
2006-12-23 13:53:16 +0000

    Fixed empty subobjects when failure.

--- tp/client/cache.py
+++ tp/client/cache.py
@@ -488,10 +488,11 @@ class Cache(object):
 			if failed(result):
 				c(sb, "failure", \
 					message="Failed to get %s for %s (id: %s)..." % (sb, str(frame.name), frame.id))
+				result = []
 			else:
 				c(sb, "downloaded", amount=1, \
 					message="Got %i %s for %s (id: %s)..." % (len(result), sb, str(frame.name), frame.id))
-				getattr(self, sb)[id] = (cache(id).modify_time, result)
+			getattr(self, sb)[id] = (cache(id).modify_time, result)
 
 		connection.setblocking(False)
 		c(sb, "finished", message="Gotten all the %s.." % sb)

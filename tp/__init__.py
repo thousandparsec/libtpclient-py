08f6ac4d0b681a102de411a58de97120ab8d6f3e
Tim Ansell <tim@thousandparsec.net>
2007-02-23 10:16:52 +0000

    Pkg_resources fixes.
    Fixes bug [1666745]. Should also work in cases where pkg_resources doesn't exist.

--- tp/__init__.py
+++ tp/__init__.py
@@ -1,5 +1,15 @@
 try:
-	__import__('pkg_resources').declare_namespace(__name__)
+	try:
+		import pkg_resources
+		pkg_resources.declare_namespace(__name__)
+	except ImportError:
+		import pkgutil
+		__path__ = pkgutil.extend_path(__path__, __name__)
+except Exception, e:
+	import warnings
+	warnings.warn(e, RuntimeWarning)
+
+try:
 	import modulefinder
 	for p in __path__:
 		modulefinder.AddPackagePath(__name__, p)

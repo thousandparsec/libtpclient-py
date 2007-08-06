6ccfe8d757b7e2a1f9b925e0df920860308686e0
Tim Ansell <mithro@mithis.com>
2007-08-06 15:43:54 +0930

    Register a _ function if one doesn't exist.

--- tp/client/__init__.py
+++ tp/client/__init__.py
@@ -2,3 +2,12 @@
 from version import version, installpath
 __version__     = version
 __installpath__ = installpath
+
+import __builtin__
+try:
+	print __builtin__
+	__builtin__._
+except AttributeError:
+	def _(s):
+		return s
+	__builtin__._ = _

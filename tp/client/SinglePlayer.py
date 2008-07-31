44d7806f6dbd446ececb4d4e25bd2e2420dfcd58
Aaron Mavrinac <aaron@riddick.(none)>
2008-07-31 13:02:31 -0400

    Added AI client list and server start functionality.

--- tp/client/SinglePlayer.py
+++ tp/client/SinglePlayer.py
@@ -78,20 +78,98 @@ class ServerList(dict):
 						'commandstring' : rparam.find('commandstring').text \
 						}
 
+class AIList(dict):
+	"""\
+	Builds a list of AIs from multiple XML files.
+	Includes rulesets and special parameters.
+	"""
+
+	def absorb_xml(self, xmlfile):
+		"""\
+		Import an XML file describing a server or server component.
+		"""
+		xmltree = ET.parse(xmlfile)
+		for aiclient in xmltree.findall('aiclient'):
+			ainame = aiclient.attrib['name']
+			if not self.has_key(sname):
+				self[ainame] = {}
+				self[ainame]['rules'] = []
+				self[ainame]['parameters'] = {}
+			for rules in aiclient.findall('rules'):
+				self[ainame]['rules'].append(rules.text)
+			for aiparam in aiclient.findall('parameter'):
+				pname = aiparam.attrib['name']
+				self[ainame]['parameters'][pname] = { \
+					'type' : aiparam.attrib['type'], \
+					'longname' : aiparam.find('longname').text, \
+					'description' : aiparam.find('description').text, \
+					'default' : aiparam.find('default').text, \
+					'commandstring' : aiparam.find('commandstring').text \
+					}
+
+
 class SinglePlayerGame:
 	"""\
 	A single-player game manager.
 	"""
+
 	def __init__(self):
 		# build a server list
-		self.servers = ServerList()
+		self.serverlist = ServerList()
 		for xmlfile in os.listdir(os.path.join(sharedir, 'servers')):
 			xmlfile = os.path.join(sharedir, 'servers', xmlfile)
 			if os.path.isfile(xmlfile) and xmlfile.endswith('xml'):
-				self.servers.absorb_xml(xmlfile)
+				self.serverlist.absorb_xml(xmlfile)
+		# build an AI client list
+		self.ailsit = AIList()
+		for xmlfile in os.listdir(os.path.join(sharedir, 'aiclients')):
+			xmlfile = os.path.join(sharedir, 'aiclients', xmlfile)
+			if os.path.isfile(xmlfile) and xmlfile.endswith('xml'):
+				self.ailist.absorb_xml(xmlfile)
+		# prepare internals
+		aiclients = []
 
-	def start(self, sname, port, sparams, ruleset, rparams, ainame, ainum, aiparams):
+	def add_aiclient(self, ainame, aiparams):
+		"""\
+		Adds an AI client to the game (before starting).
+		"""
 		pass
+
+	def start(self, sname, sparams, rname, rparams):
+		"""\
+		Starts the server and AI clients.
+		Returns True if successful (OK to connect).
+		"""
+		# start server
+		servercmd = os.path.join(sharedir, 'servers', sname + '.init') + ' ' + str(port) + ' ' + rname
+		for pname in self.serverlist[sname]['parameters'].keys():
+			if sparams.has_key(pname):
+				value = sparams[pname]
+			elif self.serverlist[sname]['parameters'][pname]['default'] != None:
+				value = self.serverlist[sname]['parameters'][pname]['default']
+			else:
+				continue
+			servercmd += ' ' + self.serverlist[sname]['parameters'][sparam]['commandstring'] % value
+		for pname in self.serverlist[sname]['rulesets'][rname]['parameters'].keys():
+			if rparams.has_key(pname):
+				value = rparams[pname]
+			elif self.serverlist[sname]['rulesets'][rname]['parameters'][pname]['default'] != None:
+				value = value = self.serverlist[sname]['rulesets'][rname]['parameters'][pname]['default']
+			else:
+				continue
+			servercmd += ' ' + self.serverlist[sname]['rulesets'][rname]['parameters'][rparam]['commandstring'] % value
+		if not os.system(servercmd) == 0:
+			return False
+
+		# start AI clients
+		for aiclient in aiclients:
+			pass
+
+		return True
 	
 	def stop(self):
+		"""\
+		Stops the server and AI clients.
+		Should be called by the client when disconnecting/closing.
+		"""
 		pass

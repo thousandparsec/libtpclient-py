00cafc5980057e459e14d34848642be99fdbcb4a
Vincent Verhoeven <verhoevenv@gmail.com>
2010-08-12 18:35:54 +0200

    Add number of players to server for singleplayer.

--- tp/client/SinglePlayer.py
+++ tp/client/SinglePlayer.py
@@ -570,6 +570,9 @@ class SinglePlayerGame:
 		port = s.getsockname()[1]
 		s.close()
 
+		# calculate the amount of players so the server can wait for them
+		numplayers = len(self.opponents) + 1
+
 		try:
 			# start server
 			server = self.locallist['server'][self.sname]
@@ -593,6 +596,7 @@ class SinglePlayerGame:
 			servercmd = servercmd % {
 						'rname' : self.rname,
 						'port' : port,
+						'numplayers' : numplayers,
 					}
 
 			# start server - add regular parameters to command line
